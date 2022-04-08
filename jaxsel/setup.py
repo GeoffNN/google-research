dee124cf5f52fabfd2be2d8588e29918a6fc0678
Geoffrey Negiar <g.negiar@gmail.com>
2022-04-07 17:31:13 -0700

    Fixed setup

--- jaxsel/setup.py
+++ jaxsel/setup.py
@@ -1,19 +1,22 @@
-
 """Install Jaxsel package."""
-import os
 import setuptools
 
-
-# Read in requirements
-with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
-  requirements = [r.strip() for r in f]
-
 setuptools.setup(
-    name='jaxsel',
-    version='0.0.1',
-    license='Apache 2.0',
-    install_requires=requirements,
-    url='https://github.com/google-research/google-research/'
-    'tree/master/jaxsel',
+    name="jaxsel",
+    version="0.0.1",
+    license="Apache 2.0",
+    install_requires=[
+        "jax>=0.3.4",
+        "numpy>=1.21",
+        "flax>=0.4",
+        "matplotlib>=3.5",
+        "tensorflow>=2.8",
+        "tensorflow-datasets>=4.5",
+        "optax>=0.1.1",
+        "tree-math>=0.1",
+        "lra>=0.0.2",
+    ],
+    url="https://github.com/google-research/google-research/" "tree/master/jaxsel",
     packages=setuptools.find_packages(),
-    python_requires='>=3.7')
+    python_requires=">=3.7",
+)
