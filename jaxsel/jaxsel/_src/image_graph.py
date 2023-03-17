6e1166f8634f60aae19d78b15703a1343f828ab9
Geoffrey Negiar <g.negiar@gmail.com>
2023-03-16 19:19:12 -0700

    Added PatchImageGraph object -- the nodes correspond to non overlapping patches in the image, instead of individual pixels

--- jaxsel/jaxsel/_src/image_graph.py
+++ jaxsel/jaxsel/_src/image_graph.py
@@ -192,7 +192,7 @@ class ImageGraph(graph_api.GraphAPI):
     Returns:
       patch: Flattened window around the start node, used as node features.
     """
-    return jnp.concatenate([self.node_patch(self._start_node_id), jnp.array([self._start_node_id])], axis=None)
+    return self.node_features(self._start_node_id)
 
   def _outgoing_edges_out_of_bounds_pixel(self, node_id, relation_ids):
     del node_id
@@ -254,3 +254,212 @@ class ImageGraph(graph_api.GraphAPI):
         None)
 
     return relation_ids, neighbor_ids
+
+
+@flax.struct.dataclass
+class PatchImageGraph(graph_api.GraphAPI):
+  """Graph representation of an image, by splitting the image in non-overlapping patches.
+  
+  In this representation, each node in the graph is a patch. 
+  Each patch is connected to its 8 neighbors, or to a "border" node.
+  """
+
+  image: np.ndarray
+  _start_node_id: int
+
+  image_shape: Tuple[int, int] = flax.struct.field(pytree_node=False)
+  patch_image_shape: Tuple[int, int] = flax.struct.field(pytree_node=False)
+  patch_size: int = flax.struct.field(pytree_node=False)
+  padding_value: int = flax.struct.field(pytree_node=False)
+  window_size: int = flax.struct.field(pytree_node=False)
+  _num_colors: int = flax.struct.field(pytree_node=False)
+
+  # pylint: disable=g-complex-comprehension
+  RELATION_OFFSETS = [
+      (i, j) for i in [-1, 0, 1] for j in [-1, 0, 1] if not i == j == 0
+  ] 
+
+  @staticmethod
+  def _pixel_coords_to_patch_coords(pixel_coords: jnp.array, patch_size: int):
+    """Converts pixel coordinates to patch coordinates.
+
+    Args:
+      pixel_coords: Pixel coordinates to convert.
+      patch_size: Size of the patches.
+
+    Returns:
+      Patch coordinates.
+    """
+    return (jnp.array(pixel_coords) // jnp.array([patch_size, patch_size])).astype(int)
+
+
+  def sample_start_node_id(self, seed=0):
+    del seed  # Currently assuming there's only a single start node.
+    return self._start_node_id
+
+  def _patch_coordinates_to_id(self, node_hw):
+    node_id = self.patch_image_shape[1] * node_hw[0] + node_hw[1]
+    return jnp.where(node_id < 0, -1, node_id)
+
+  def _patch_id_to_coordinates(self, node_id):
+    """Converts pixel id to coordinates.
+
+    Args:
+      pixel_id: id of the pixel.
+
+    Returns:
+      coordinates: coordinates of the pixel in the image. (-1, -1) for the
+        'pixel' with id -1.
+    """
+    # Can't use jnp.unravel_index due to the -1 node.
+    coordinates = jnp.array(
+        (node_id // self.patch_image_shape[1], node_id % self.patch_image_shape[1]))
+    coordinates = jnp.where(
+        jnp.all(coordinates >= 0, axis=-1, keepdims=True), coordinates, -1)
+    return coordinates
+
+  @staticmethod
+  def create(image,
+             get_start_pixel_fn,
+             patch_size = 3,
+             num_colors = 10,
+             padding_value = 0):
+    """Constructor based on an integer image.
+
+    Args:
+      image: The image to be represented as a graph.
+      get_start_pixel_fn: This function produces the location of the start
+        patch as a pair of (h, w) coordinates.
+      patch_size: Size of patches to use as nodes.
+      num_colors: Number of colors in the image.
+      padding_value: Value to insert on the boundary of the pixel for computing
+        patch features of pixels on the boundary.
+
+    Returns:
+      Corresponding graph
+    """
+    if image.ndim != 2:  # Only handle single-channel for now.
+      raise ValueError(
+          f'image must be a 2-d object. Got {image.ndim}D instead.')
+    if patch_size % 2 != 1:  # Require odd-sized patches for symmetry.
+      raise ValueError(
+          f'Patch must be odd-sized for symmetry. Got {patch_size}.')
+    window_size = (patch_size - 1) // 2
+    
+    patch_sizes = jnp.array([patch_size, patch_size])
+    image = jnp.pad(image, 
+                      jnp.vstack([jnp.ceil(jnp.array(image.shape) / patch_sizes) * patch_sizes - jnp.array(image.shape), jnp.zeros(2)]).T.astype(int),
+                      mode='constant')
+
+    patch_image_shape = jnp.array(image.shape) // patch_sizes
+    start_node_id = np.ravel_multi_index(PatchImageGraph._pixel_coords_to_patch_coords(get_start_pixel_fn(image), patch_size), patch_image_shape)
+
+    return PatchImageGraph(
+        image=image,
+        image_shape=image.shape,
+        patch_size=patch_size,
+        patch_image_shape=patch_image_shape,
+        padding_value=padding_value,
+        window_size=window_size,
+        _start_node_id=start_node_id,
+        _num_colors=num_colors)
+
+  def node_metadata(self, node_id):
+    return {'coordinates': self._patch_id_to_coordinates(node_id)}
+  
+  def node_patch(self, node_id):
+    """Returns the features for a given node.
+    
+    The node is represented by a patch of the image.
+    """
+    patch_coordinates = self._patch_id_to_coordinates(node_id)
+    center_pixel_coordinates = patch_coordinates * self.patch_size + self.window_size
+    patch = _get_window(center_pixel_coordinates, self.image, self.window_size, padding_value=self.padding_value)
+    return patch
+  
+  def node_features(self, node_id):
+    return jnp.concatenate([self.node_patch(node_id), jnp.array([node_id])], axis=None)
+  
+  def graph_parameters(self):
+    return graph_api.GraphParameters(
+        node_vocab_size=self._num_colors,
+        num_relation_types=len(self.RELATION_OFFSETS),
+        node_feature_dim=self.patch_size**2,
+        node_feature_kind=graph_api.NodeFeatureKind.CATEGORICAL,
+        task_vocab_size=self._num_colors,
+        task_feature_dim=self.patch_size**2,
+        task_feature_kind=graph_api.NodeFeatureKind.CATEGORICAL)
+
+
+  def _outgoing_edges_out_of_bounds_pixel(self, node_id, relation_ids):
+    del node_id
+    return jnp.full_like(relation_ids, self._start_node_id)
+
+  def _outgoing_edges_in_bounds_pixel(self, node_id, relation_ids):
+    """Returns the outgoing edges from `node_id`.
+
+    Patches outside of the image are mapped to node_id -1.
+    This "out of bounds" node points to the start node.
+    The considered initial node must be inside the image bounds.
+
+    Args:
+      node_id: ID of start node
+      relation_ids: IDs of the relations to neighbors.
+
+    Returns:
+      A list of neighbor ids, in the order given by relation_ids.
+    """
+    node_coordinates = self._patch_id_to_coordinates(node_id)
+    neighbor_coordinates = jnp.repeat(
+        jnp.array(node_coordinates)[jnp.newaxis, :], len(self.RELATION_OFFSETS),
+        0)
+    offsets = jnp.array(self.RELATION_OFFSETS)
+    neighbor_coordinates = neighbor_coordinates + offsets
+    # make sure coords are within bounds, or -1
+    neighbor_coordinates = jnp.where(
+        jnp.less_equal(neighbor_coordinates,
+                       jnp.array(self.patch_image_shape) - 1), neighbor_coordinates,
+        -1)
+    # make sure coordinates are >= 0, or -1
+    neighbor_coordinates = jnp.where(
+        jnp.all(neighbor_coordinates >= 0, axis=-1, keepdims=True),
+        neighbor_coordinates, -1)
+
+    neighbor_ids = jax.vmap(self._patch_coordinates_to_id)(neighbor_coordinates)
+    return neighbor_ids
+
+  def outgoing_edges(self, node_id):
+    """Returns the outgoing edges from `node_id`.
+
+    Pixels outside of the image are mapped to node_id -1.
+    This "out of bounds pixel" node points to the start node.
+
+    Args:
+      node_id: ID of start node
+
+    Returns:
+       A list of relation ids and neighbor ids. The relation id is that of
+       the edge from the initial node to the neighbor.
+    """
+    relation_ids = jnp.arange(len(self.RELATION_OFFSETS))
+
+    neighbor_ids = jax.lax.cond(
+        node_id == -1,
+        lambda _: self._outgoing_edges_out_of_bounds_pixel(  # pylint: disable=g-long-lambda
+            node_id, relation_ids),
+        lambda _: self._outgoing_edges_in_bounds_pixel(node_id, relation_ids),
+        None)
+
+    return relation_ids, neighbor_ids
+
+  
+  @property
+  def start_node_coords(self):
+    return self.node_metadata(self._start_node_id)['coordinates']
+  
+  def sample_start_node_id(self, seed=0):
+    del seed
+    return self._start_node_id
+  
+  def task_features(self):
+    return self.node_features(self._start_node_id) 
